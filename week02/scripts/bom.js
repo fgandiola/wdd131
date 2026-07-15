e3c26222d929d8b8cb3d2da9a7152de0ba2eefe1
fgandiola <fgandiola@byupathway.edu>
2026-07-15 18:46:53 -0300

    w2 learning act 3

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,19 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button')
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus()
+        });
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.value = '';
+    }
+});
