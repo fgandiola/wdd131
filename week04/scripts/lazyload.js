fe2d36ce399acd10c411146acf65df9f054b0cd8
fgandiola <fgandiola@byupathway.edu>
2026-07-27 17:52:38 -0300

    w4 learning 2

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+const currentyear = document.querySelector("#current-year")
+const lastModified = document.querySelector("#lastModified")
+const today = new Date()
+currentyear.innerHTML = today.getFullYear()
+document.getElementById("lastModified").innerHTML = document.lastModified;
