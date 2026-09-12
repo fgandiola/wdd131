78458abf31450d3f9c6b715c6fdd716556614fa4
fgandiola <fgandiola@byupathway.edu>
2026-09-12 17:28:00 -0300

    09-12, w2, album 2

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,12 @@ const year = document.querySelector("#currentYear");
 const today = new Date();
 year.innerHTML = today.getFullYear()
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`
+
+// navigation button
+
+const navButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+navButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    navButton.classList.toggle("open");
+});
